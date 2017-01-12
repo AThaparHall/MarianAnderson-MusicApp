2047ff5c76e517dee0d1f56b7b773b9774e49b0a
Avneet <thaparlym@gmail.com>
2017-01-12 08:03:26 -0500

    minor changes

--- MarianAnderson/app/src/main/java/com/example/android/mariananderson/Spirituals.java
+++ MarianAnderson/app/src/main/java/com/example/android/mariananderson/Spirituals.java
@@ -1,13 +1,48 @@
 package com.example.android.mariananderson;
 
+import android.content.Intent;
+import android.media.Image;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
-public class Spirituals extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.example.android.mariananderson.R.id.avemaria;
+import static com.example.android.mariananderson.R.id.spirituals;
+
+public class Spirituals extends AppCompatActivity  {
+
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final ImageView play = (ImageView) findViewById(R.id.avemaria);
+
         setContentView(R.layout.activity_spirituals);
-    }
-}
+        mediaPlayer = MediaPlayer.create(this, R.raw.avemaria);
+
+        // Find the View that shows the Ave Maria View
+
+        // Set a click listener on that View
+        play.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the AveMaria View is clicked on.
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(this, "Playing sound",Toast.LENGTH_SHORT).show();
+                        mediaPlayer.start();
+            }
+        })
+            }
+        }
+
